455c1f41760dbcd82c3582791bac2cc9a924956d
vinlydev <vanhsana1737@gmail.com>
2021-08-21 19:34:54 +0700

    hot fix

--- lib/pages/LoginPage.dart
+++ lib/pages/LoginPage.dart
@@ -1,6 +1,6 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
-import 'package:intl/intl.dart';
 
 class LoginPage extends StatefulWidget {
   LoginPage({Key key}) : super(key: key);
@@ -21,6 +21,49 @@ class _LoginPageState extends State<LoginPage> {
               begin: Alignment.topRight,
               end: Alignment.bottomLeft,
               colors: [Colors.white70, Colors.blue])),
+      child: Center(
+        child: SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              Image(
+                image: AssetImage('assets/img/kg.png'),
+                height: 50,
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: FormBuilder(
+                  key: _formKey,
+                  initialValue: {
+                    'email': '',
+                    'password': '',
+                  },
+                  autovalidateMode: AutovalidateMode.always,
+                  child: Column(
+                    children: <Widget>[
+                      FormBuilderTextField(
+                        name: 'email',
+                        maxLines: 1,
+                        decoration: InputDecoration(
+                            labelText: 'Email',
+                            labelStyle: TextStyle(color: Colors.black87),
+                            fillColor: Colors.white30,
+                            filled: true,
+                            errorStyle: TextStyle(color: Colors.white)),
+                        validator: FormBuilderValidators.compose([
+                          FormBuilderValidators.required(context,
+                              errorText: 'ກະລຸນາໃສ່ອີເມວ'),
+                          FormBuilderValidators.email(context,
+                              errorText: 'ກະລຸນາກວດເບິ່ງອີເມວຄືນ')
+                        ]),
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
     ));
   }
 }
