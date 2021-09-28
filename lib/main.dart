98a704867ac9943c2b61aee2ecdfb7062c12f656
vinlydev <89177679+vinlydev@users.noreply.github.com>
2021-09-28 19:04:55 +0700

    hotfix

--- lib/main.dart
+++ lib/main.dart
@@ -40,11 +40,12 @@ class MyApp extends StatelessWidget {
           theme: ThemeData(
               // primarySwatch: Colors.blue,
               primaryColor: Colors.blue,
+              // ignore: deprecated_member_use
               accentColor: Colors.blueAccent,
               canvasColor: Colors.blue[50],
               textTheme: TextTheme(
                 // ignore: deprecated_member_use
-                headline: TextStyle(color: Colors.blue, fontSize: 30),
+                headline1: TextStyle(color: Colors.blue, fontSize: 30),
               )),
           // home: HomePage(),
           initialRoute: '/',
