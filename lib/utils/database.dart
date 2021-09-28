3020fb1a88053eeef79567fd4a7afce52a8634f2
vinlydev <89177679+vinlydev@users.noreply.github.com>
2021-09-28 13:17:12 +0700

    hotfix

---
+++ lib/utils/database.dart
@@ -0,0 +1,27 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DBHelper {
+  static Database _db;
+
+  Future<Database> get db async {
+    if (_db != null) {
+      return _db;
+    }
+    _db = await initDatabase();
+    return _db;
+  }
+
+  Future<Database> initDatabase() async {
+    var databasesPath = await getDatabasesPath();
+    String path = join(databasesPath, 'customer.db');
+
+    Database db = await openDatabase(path, version: 1,
+        onCreate: (Database db, int version) async {
+      // When creating the db, create the table
+      await db.execute(
+          'CREATE TABLE customers (id INTEGER PRIMARY KEY, name TEXT, phone INTEGER)');
+    });
+    return db;
+  }
+}
