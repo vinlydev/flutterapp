e2b581ed50af82be09959865c6133be703129032
vinlydev <89177679+vinlydev@users.noreply.github.com>
2021-09-29 19:49:15 +0700

    hotfix

--- lib/pages/CameraPage.dart
+++ lib/pages/CameraPage.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutterapp/widgets/menu.dart';
+import 'package:camera/camera.dart';
+import 'package:path_provider/path_provider.dart';
 
 class CameraPage extends StatefulWidget {
   CameraPage({Key key}) : super(key: key);
@@ -9,19 +13,55 @@ class CameraPage extends StatefulWidget {
 }
 
 class _CameraPageState extends State<CameraPage> {
+  CameraController _controller;
+  List<CameraDescription> cameras;
+  CameraDescription camera;
+  Widget cameraPreview;
+
+  Future<void> initCamera() async {
+    cameras = await availableCameras();
+    camera = cameras.first;
+    _controller = CameraController(camera, ResolutionPreset.ultraHigh);
+
+    await _controller.initialize();
+
+    cameraPreview = Center(
+      child: CameraPreview(_controller),
+    );
+
+    setState(() {
+      cameraPreview = cameraPreview;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    initCamera();
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Menu(),
-      appBar: AppBar(title: Text('ຖ່າຍຮູບ')),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('ກ້ອງຖ່າຍຮູບ'),
+        drawer: Menu(),
+        appBar: AppBar(
+          title: Text('ຖ່າຍຮູບ'),
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.camera_enhance),
+              onPressed: () async {
+                Directory tempDir = await getTemporaryDirectory();
+                // await _controller.takePicture(tempDir.path);
+              },
+            )
           ],
         ),
-      ),
-    );
+        body: Container(child: cameraPreview));
   }
 }
